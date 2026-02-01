6a4fa97ad8bdb762228977ba1b85482fb08e3694
Francisco Sebastián Ibarra Rojas <franciscoibarrarojas20@gmail.com>
2026-01-31 23:07:49 -0400

    Agregado el efecto del boton

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ $(document).ready(function() {
         $('body').toggleClass('bg-dark text-white');
     });
 });
+$('#email').on('input', function() {
+        const email = $(this).val();
+        if (email.includes('@') && email.includes('.')) {
+            $('#emailFeedback').text('Email válido').removeClass('text-danger').addClass('text-success');
+        } else {
+            $('#emailFeedback').text('Ingresa un email válido').removeClass('text-success').addClass('text-danger');
+        }
+    });
+
+    $('h2').on('mouseenter', function() {
+        $(this).css('color', 'blue');
+    }).on('mouseleave', function() {
+        $(this).css('color', '');
+    });
