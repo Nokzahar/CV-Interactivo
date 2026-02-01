16e0fd308c563f8c33ae2c768f59ff748904c150
Francisco Sebastián Ibarra Rojas <franciscoibarrarojas20@gmail.com>
2026-01-31 23:05:05 -0400

    Funcion de comprobacion de correo

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // 1. Cambio de tema Claro/Oscuro (Requerimiento 3.1)
     $('#btnTheme').click(function() {
         $('body').toggleClass('bg-dark text-white');
     });
