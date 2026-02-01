587561bcc32de77b617b8d661e0b0c4d74e8cc30
Francisco Sebastián Ibarra Rojas <franciscoibarrarojas20@gmail.com>
2026-02-01 00:05:56 -0400

    Agregada la opción de enviar mensaje y comprobacion de correo

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-$(document).ready(function() {
-    // 1. Cambio de tema Claro/Oscuro (Requerimiento 3.1)
+$(document).ready(function() {    // Funcion para alternar tema oscuro y claro
     $('#btnTheme').click(function() {
         $('body').toggleClass('bg-dark text-white');
     });
-});
-$('#email').on('input', function() {
+    });
+    $('#email').on('input', function() {  // Funcion para validar correo
         const email = $(this).val();
         if (email.includes('@') && email.includes('.')) {
             $('#emailFeedback').text('Email válido').removeClass('text-danger').addClass('text-success');
         } else {
             $('#emailFeedback').text('Ingresa un email válido').removeClass('text-success').addClass('text-danger');
         }
+        $(document).ready(function() {
+
+
+$('#contactForm').on('submit', function(e) {
+    e.preventDefault(); // Codigo para evitar que la página se recargue
+        
+    const email = $('#email').val();
+        
+    if (email.includes('@') && email.includes('.')) {
+        $(this).html('<div class="alert alert-success mt-3">' +
+                         '<strong>¡Enviado!</strong> Tu mensaje se ha enviado correctamente.' +
+                         '</div>');
+        console.log("Formulario enviado con éxito por: " + email);
+    } else {
+        alert("Por favor, ingresa un correo válido antes de enviar.");
+    }
+});
+
     });
 
-    $('h2').on('mouseenter', function() {
-        $(this).css('color', 'blue');
+    $('h2').on('mouseenter', function() {  // Funcion para efecto de cambio de color de texto al pasar el mouse por encima
+        $(this).css('color', 'grey');
     }).on('mouseleave', function() {
         $(this).css('color', '');
     });
+});
